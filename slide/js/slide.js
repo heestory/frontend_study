01a79d6ec846984fc1b4f4376d8950916ba71be2
jeonghee <jeongh389@gmail.com>
2017-12-27 22:01:29 +0900

    lite.js load

--- slide/js/slide.js
+++ slide/js/slide.js
@@ -1,18 +1,2 @@
-(function(win){
 
-    var global = win;
-    var doc = this.document;
-
-    var lite = function(params, context){
-      return new GetOrMakeDom(params, context);
-    };
-
-    var GetOrMakeDom = function(params, context){
-
-    };
-
-    global.lite = lite;
-
-    lite.fn = GetOrMakeDom.prototype;
-
-})(window);
+console.log(lite('ul').append('<li>3</li>'));
