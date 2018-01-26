4dcdac49dea39a3e538c831bbbb2463444da94cc
jeonghee <jeongh389@gmail.com>
2018-01-26 17:54:57 +0900

    create buttons

--- slide/lite.js
+++ slide/lite.js
@@ -100,7 +100,13 @@ lite.fn.append = function(stringOrObject){
 lite.fn.createSlide = function(picArray, width){
 	
 	var div = this.createSlideTemplate(picArray, width);
-	this.append(div);
+	var buttons = this.createButton();
+	//this.append(div);
+	this.append(buttons);
+
+	this.listenButtonEvent(document.querySelector("div[name='rightButton']"));
+	this.listenButtonEvent(document.querySelector("div[name='leftButton']"));
+
 };
 
 lite.fn.createSlideTemplate = function(picArray, width){
@@ -112,5 +118,97 @@ lite.fn.createSlideTemplate = function(picArray, width){
 			
 	}
 	return html+"</div>";
+}
+lite.fn.createButton = function(){
+	var div = document.createElement('div');
+	div.style.width = '40px';
+	div.style.height = '40px';
+	div.style.border = '1px solid';
+    div.style.position = 'absolute';
+
+	var buttonArray = [];
+	var clickCount = 0;
+	
+	var rbutton = div.cloneNode(true);
+	rbutton.style.right = '0';
+	var rtext = document.createTextNode('right');
+	rbutton.appendChild(rtext);
+
+	var lbutton = div.cloneNode(true);
+	var ltext = document.createTextNode('left');
+	lbutton.appendChild(ltext);
+	
+	this.setAttributes(lbutton,{'name':'leftButton', 'draggable':'true', 'ondragstart':'drag(event)','id':'leftButton'});
+	this.setAttributes(rbutton,{'name':'rightButton'});
+	
+
+	buttonArray.push(rbutton);
+	buttonArray.push(lbutton);
+
+	return buttonArray;
+}
+
+lite.fn.listenButtonEvent= function(el){
+	var clickCount = 0;
+	var dbFlag = false;
+	el.addEventListener('click',function(){
+			
+			clickCount++;
+
+			if(clickCount === 1){
+			    singleClickTimer = setTimeout(function() {
+		            clickCount = 0;
+		            //@TODO
+		        }, 400);
+
+			}else if(clickCount === 2){
+
+				clearTimeout(singleClickTimer);
+		        clickCount = 0;
+		
+				dbFlag == true ? el.style.backgroundColor = 'green' : el.style.backgroundColor = 'white';
+				dbFlag == true ? el.classList.add("on") : el.classList.remove("on");
+				dbFlag == true ? dbFlag = false : dbFlag = true;
+				
+			}
+
+		
+
+	});
+
+}
+
+lite.fn.setAttributes = function(el,attrs){
+
+	for(var key in attrs){
+		el.setAttribute(key, attrs[key]);
+	}
+	
+}
+
+function allowDrop(ev) {
+
+    ev.preventDefault();
+    return false;
+}
+
+function drag(event) {
+	
+	var style = window.getComputedStyle(event.target, null);
+    var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY)+ ',' + event.target.id;
+    event.dataTransfer.setData("Text",str);
+    
+}
+
+function drop(event) {
+	// if(event.target.getAttribute('class') != 'on'){
+	// 	return;
+	// }
+     var offset = event.dataTransfer.getData("Text").split(',');
+    var dm = document.getElementById(offset[2]);
+    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    event.preventDefault();
+    return false;
 }
 
